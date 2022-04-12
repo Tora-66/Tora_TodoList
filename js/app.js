4c225a0f772ba008cd5db0c617c50a56baef134e
Hua Quang <huaquang137@gmail.com>
2022-04-12 23:00:56 +0700

    Add main files

---
+++ js/app.js
@@ -0,0 +1,15 @@
+//Selectors
+const todoInput = document.querySelector(".todo-input");
+const todoButton = document.querySelector(".todo-button");
+const todoList = document.querySelector(".todo-list");
+
+//Event listeners
+todoButton.addEventListener('click',addTodo);
+
+//Functions
+function addTodo(event) {
+    //Prevent form from submitting
+    event.preventDefault();
+    //todo DIV
+    
+}
