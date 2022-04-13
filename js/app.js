39310d0107bea3226ae7008820a1892aaa00ec46
Hua Quang <huaquang137@gmail.com>
2022-04-13 13:09:10 +0700

    Update function

--- js/app.js
+++ js/app.js
@@ -5,11 +5,50 @@ const todoList = document.querySelector(".todo-list");
 
 //Event listeners
 todoButton.addEventListener('click',addTodo);
+todoList.addEventListener('click', deleteCheck);
 
 //Functions
 function addTodo(event) {
     //Prevent form from submitting
     event.preventDefault();
     //todo DIV
-    
+    const todoDiv = document.createElement("div");
+    todoDiv.classList.add("todo");
+    //Create LI
+    const newTodo = document.createElement("li");
+    newTodo.innerText = todoInput.value;
+    newTodo.classList.add("todo-item");
+    todoDiv.appendChild(newTodo);
+    //check MARK BUTTON
+    const completedButton = document.createElement("button");
+    completedButton.innerHTML = '<i class="fas fa-check"></i>';
+    completedButton.classList.add("complete-btn");
+    todoDiv.appendChild(completedButton);
+    //CHECK trash BUTTON
+    const trashButton = document.createElement("button");
+    trashButton.innerHTML = '<i class="fas fa-trash"></i>';
+    trashButton.classList.add("trash-btn");
+    todoDiv.appendChild(trashButton);
+    //APPEND to list
+    todoList.appendChild(todoDiv);
+}
+
+function deleteCheck(e) {
+    const item = e.target;
+    //DETELE todo
+    if (item.classList[0] === "trash-btn") {
+        const todo = item.parentElement;
+        //Animation
+        todo.classList.add("fall");
+        todo.addEventListener("transitionend", function(){
+            todo.remove();
+        });
+    }
+
+    //CHECK MARK
+    if (item.classList[0] === "complete-btn") {
+        const todo = item.parentElement;
+        todo.classList.toggle("completed");
+    }
+
 }
